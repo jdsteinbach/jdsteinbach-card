6b175c34236f8592fe67f89576fcbb2064aef01c
James Steinbach <jdsteinbach@gmail.com>
2018-12-29 14:50:10 -0700

    add some colors & a different border

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
-const head = [
+const chalk = require('chalk')
+
+// Faceshot art
+const face = [
 '   /////////',
 '  /ʗ/---▭-▭ ',
 '  \\\\\\     ᔨ ',
@@ -9,22 +12,49 @@ const head = [
 ' /  \\    \\  ',
 ]
 
+// Text Strings
 const text = [
-  'James Steinbach',
-  'https://jamessteinbach.com',
-  'https://jdsteinbach.com',
-  'https://github.com/jdsteinbach',
-  'https://twitter.com/jdsteinbach',
-  'https://linkedin.com/jdsteinbach'
+  {
+    style: 'title',
+    text: 'James Steinbach',
+  },
+  {
+    style: 'title',
+    text: 'Senior Front-End Architect'
+  },
+  {
+    style: 'link',
+    text: 'https://jamessteinbach.com',
+  },
+  {
+    style: 'link',
+    text: 'https://github.com/jdsteinbach',
+  },
+  {
+    style: 'link',
+    text: 'https://twitter.com/jdsteinbach',
+  },
+  {
+    style: 'link',
+    text: 'https://linkedin.com/jdsteinbach',
+  },
 ]
 
-console.log([
-  `╭${'─'.repeat(54)}╮`,
-  `│${' '.repeat(54)}│`,
+// Styles
+const s = {
+  link: str => chalk.hex('#5C6BC0')(str),
+  title: str => chalk.bold.hex('#5C6BC0')(str),
+  border: str => chalk.bgHex('#546E7A')(str),
+  face: str => chalk.hex('#FB8C00')(str)
+}
+
+console.log(chalk.bgHex('#263238')([
+  s.border(' ').repeat(60),
+  `${s.border('  ')}${' '.repeat(56)}${s.border('  ')}`,
   text
-    .map((text, i) => `│ ${head[i] || ' '.repeat(12)}${' '.repeat(39 - text.length)}${text}  │`)
+    .map((t, i) => `${s.border('  ')}  ${s.face(face[i]) || ' '.repeat(12)}${' '.repeat(39 - t.text.length)}${s[t.style](t.text)}   ${s.border('  ')}`)
     .join('\n'),
-  `│${' '.repeat(54)}│`,
-  `╰${'─'.repeat(54)}╯`,
-].join('\n'))
+  `${s.border('  ')}${' '.repeat(56)}${s.border('  ')}`,
+  s.border(' ').repeat(60)
+].join('\n')))
 
