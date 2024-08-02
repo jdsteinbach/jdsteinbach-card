76a783910ac0a18d8ba1f415f542a09f1cae562c
James Steinbach <jdsteinbach@gmail.com>
2024-08-02 11:43:25 -0600

    fix bugs

--- getNordColors.js
+++ getNordColors.js
@@ -1,11 +1,22 @@
-import { readFileSync } from 'node:fs'
-const nordSass = readFileSync(`${process.cwd()}/node_modules/nord/src/sass/nord.scss`, 'utf8')
+// colors from https://www.nordtheme.com
 
-const colorVariables = nordSass.matchAll(/\$(nord\d+)\:\s+?(\#[a-z0-9]{6};)/gim)
-
-const colors = colorVariables.reduce((acc, [whole_, k, v]) => {
-  acc[k] = v
-  return acc
-}, {})
+const colors = {
+  nord0: '#2E3440;',
+  nord1: '#3B4252;',
+  nord2: '#434C5E;',
+  nord3: '#4C566A;',
+  nord4: '#D8DEE9;',
+  nord5: '#E5E9F0;',
+  nord6: '#ECEFF4;',
+  nord7: '#8FBCBB;',
+  nord8: '#88C0D0;',
+  nord9: '#81A1C1;',
+  nord10: '#5E81AC;',
+  nord11: '#BF616A;',
+  nord12: '#D08770;',
+  nord13: '#EBCB8B;',
+  nord14: '#A3BE8C;',
+  nord15: '#B48EAD;'
+}
 
 export default colors
